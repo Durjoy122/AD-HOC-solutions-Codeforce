5768d38b57019b3a857bc3c0cabac7aad57591f8
Durjoy <77919974+Dujoy@users.noreply.github.com>
2021-05-04 11:05:09 +0600

    Delete project/project directory

--- project/project/BookOperation.java
+++
@@ -1,9 +0,0 @@
-import java.lang.*;
-
-public interface BookOperation
-{
-	public boolean insertBook(Book b);
-	public boolean removeBook(Book b);
-	public Book searchBook(String isbn2);
-	public void showAllBook();
-}
